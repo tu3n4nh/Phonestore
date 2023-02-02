73e39839c6ed99f83932dbede7664edcc933660e
AT180204 <at180204@actvn.edu.vn>
2023-02-03 01:22:39 +0700

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,85 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.imgur.com/1zVxwb3.png",
+      title: "PHONE",
+      desc: "Điện thoại iPhone đầy đủ mẫu mã, chủng loại, các kiểu con đà điểu...",
+      bg: "f5fafd",
+      cat:"phone"
+
+    },
+    {
+      id: 2,
+      img: "https://i.imgur.com/UACmygN.png",
+      title: "PHỤ KIỆN",
+      desc: "Phụ kiện, cáp sạc, tai nghe,... đa con nhà bà dạng",
+      bg: "fcf1ed",
+      cat:"accessory"
+
+    },
+    {
+      id: 3,
+      img: "https://i.imgur.com/ginTVxC.png",
+      title: "Linh kiện",
+      desc: "Màn hình, pin, main,... thoải mái lựa chọn",
+      bg: "fbf0f4",
+      cat:"accessories"
+
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.imgur.com/1zVxwb3.png",
+      title: "Phone!",
+      cat:"phone"
+    },
+    {
+      id: 2,
+      img: "https://i.imgur.com/UACmygN.png",
+      title: "Phụ kiện",
+      cat:"accessory"
+    },
+    {
+      id: 3,
+      img: "https://i.imgur.com/ginTVxC.png",
+      title: "Linh kiện",
+      cat:"accessories"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
